49d64a30c1a36cf4f1f2c4f072f0345744b37a19
Zachary Irvine <irvinezachary@gmail.com>
2023-04-11 15:06:05 -0600

    ATtempt to make burialrecord work. DOES NOT CURRENTLY FUNCTION

--- Models/ColorTextile.cs
+++ Models/ColorTextile.cs
@@ -8,6 +8,9 @@ namespace INTEXII.Models
     public partial class ColorTextile
     {
         public long MainColorid { get; set; }
+        public Color Color { get; set; }
         public long MainTextileid { get; set; }
+        public Textile Textile { get; set; }
+
     }
 }
--- Models/TextilefunctionTextile.cs
+++ Models/TextilefunctionTextile.cs
@@ -8,6 +8,8 @@ namespace INTEXII.Models
     public partial class TextilefunctionTextile
     {
         public long MainTextilefunctionid { get; set; }
+        public Textilefunction Textilefunction { get; set; }
         public long MainTextileid { get; set; }
+        public Textile Textile { get; set; }
     }
 }
