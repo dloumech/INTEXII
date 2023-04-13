6d682183cbf3a5e5b4c76cceb19140e34671488e
Katiew8 <katiejane270@gmail.com>
2023-04-12 20:17:01 -0600

    rbac

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace INTEXII.Controllers
             return View();
         }
 
+        public IActionResult AdminUser()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
